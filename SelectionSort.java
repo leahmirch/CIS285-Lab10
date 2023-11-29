29b58c3cb2e50448871b5aab1a3b075213a0b678
leahmirch <lmirch@umich.edu>
2023-11-29 11:17:42 -0500

    selection sort fixes

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,22 +1,30 @@
-// non working code
+// working code
 
 public class SelectionSort {
-	 private int temp;
+    private int temp;
 
-	 // Creates a new instance of SelectionSort
+    // Creates a new instance of SelectionSort
     public SelectionSort() {
     }
- 
+
     public int[] basicSelectionSort(int[] x) {
         for (int i = 0; i < x.length; ++i) {
-            for (int j= i+1; j < x.length; ++j) {
+            for (int j = i + 1; j < x.length; ++j) {
                 if (x[i] > x[j]) {
+                	
+                    // Swapping elements to sort
                     temp = x[i];
                     x[i] = x[j];
-                    temp = x[j];
+                    // Corrected the swapping logic
+                    x[j] = temp; // Originally was: temp = x[j];
+
+                    // Original code for reference:
+                    // temp = x[i];
+                    // x[i] = x[j];
+                    // temp = x[j]; // Incorrect swap
                 }
-            } 
-        } 
+            }
+        }
         return x;
-    } 
+    }
 }
